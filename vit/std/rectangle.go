6cbb7320a1a5dcba7370a504c11c5a04ade7d0a3
Jannis Rieger <omniskopus@gmail.com>
2022-06-22 00:04:11 +0200

    added radius to rectangle

--- vit/std/rectangle.go
+++ vit/std/rectangle.go
@@ -1,27 +1,31 @@
 package std
 
 import (
-	"fmt"
+	"math"
 
 	"github.com/omniskop/vitrum/vit"
 	"github.com/tdewolff/canvas"
 )
 
 func (r *Rectangle) Draw(ctx vit.DrawingContext, area vit.Rect) error {
-	rect := vit.Rect{
-		X1: r.left.Float64(),
-		Y1: r.top.Float64(),
-		X2: r.right.Float64(),
-		Y2: r.bottom.Float64(),
-	}
+	rect := r.Bounds()
 
 	ctx.SetFillColor(r.color.Color())
-	ctx.SetStrokeJoiner(canvas.MiterJoin)
-	ctx.SetStrokeColor(r.border.MustGet("color").(*vit.ColorValue).Color())
-	ctx.SetStrokeWidth(float64(r.border.MustGet("width").(*vit.IntValue).Int()))
-	fmt.Println(r.border.MustGet("color").(*vit.ColorValue).Color().RGBA())
-	fmt.Println(r.border.MustGet("width").(*vit.IntValue).Int())
-	ctx.DrawPath(0, 0, rect.ToCanvas().ToPath())
+
+	borderWidth := float64(r.border.MustGet("width").GetValue().(int))
+	if borderWidth > 0 {
+		ctx.SetStrokeJoiner(canvas.MiterJoin)
+		ctx.SetStrokeColor(r.border.MustGet("color").(*vit.ColorValue).Color())
+		ctx.SetStrokeWidth(float64(r.border.MustGet("width").(*vit.IntValue).Int()))
+		radius := math.Max(r.radius.Float64()-borderWidth/2, 0) // decrease radius. Make sure it can't get below 0
+		ctx.DrawPath(
+			rect.X1+borderWidth/2,
+			rect.Y1+borderWidth/2,
+			canvas.RoundedRectangle(rect.Width()-borderWidth, rect.Height()-borderWidth, radius),
+		)
+	} else {
+		ctx.DrawPath(rect.X1, rect.Y1, canvas.RoundedRectangle(rect.Width(), rect.Height(), r.radius.Float64()))
+	}
 
 	r.Root.DrawChildren(ctx, rect)
 
